925ceccd1cb6111c9e32180f49dc50d10e0580ac
221B490 <221B490@juet.co.in>
2025-02-27 14:27:05 +0530

    This is my seventh commit

---
+++ LE4/Eager Solution/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    Singleton_E S1=Singleton_E.getInstance();
+    public static void main(String[] args)
+    {
+        System.out.print("Design Pattern with singleton_E class");
+    }
+    
+}
